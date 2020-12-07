e619c5a262916e4c942cdb085d99fd7ce768acd7
johnsci911 <johnguitaizta@gmail.com>
2020-12-08 00:03:49 +0800

    Cleanup polybar

--- .config/polybar/default/launch.sh
+++ .config/polybar/default/launch.sh
@@ -7,4 +7,6 @@ killall polybar
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
 # Launch polybar
-polybar example -c $(dirname $0)/config.ini &
+polybar base -c $(dirname $0)/config.ini &
+polybar center -c $(dirname $0)/config.ini &
+polybar end -c $(dirname $0)/config.ini &
